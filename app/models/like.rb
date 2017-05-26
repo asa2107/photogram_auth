d67749b0a244dfaf56927b1624ffd011753d02f6
Abdus Samad Anwar <asa2107@gmail.com>
2017-05-26 08:33:16 -0500

    v1

--- app/models/like.rb
+++ app/models/like.rb
@@ -1,5 +1,5 @@
 class Like < ApplicationRecord
-  validates :user_id, :presence => true, :uniqueness => { :photo_id => true }
+  validates :user_id, :presence => true, :uniqueness => { :scope => :photo_id }
   validates :photo_id, :presence => true
   belongs_to :user
   belongs_to :photo
