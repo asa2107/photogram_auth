6fcc43608a87782721a11794c529dd64b4d593c9
Abdus Samad Anwar <asa2107@gmail.com>
2017-05-30 23:22:35 -0500

    a

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,35 +1,29 @@
 class CommentsController < ApplicationController
   def index
     @comments = Comment.all
-
     render("comments/index.html.erb")
   end
 
   def show
     @comment = Comment.find(params[:id])
-
     render("comments/show.html.erb")
   end
 
   def new
     @comment = Comment.new
-
     render("comments/new.html.erb")
   end
 
   def create
     @comment = Comment.new
-
-    @comment.photo_id = params[:photo_id]
+    @comment.photo_id = @photos.id
     @comment.body = params[:body]
-    @comment.user_id = params[:user_id]
-
+    @comment.user_id = current_user.id
     save_status = @comment.save
-
     if save_status == true
-      redirect_to("/comments/#{@comment.id}", :notice => "Comment created successfully.")
+      redirect_to("/photos", :notice => "Comment created successfully.")
     else
-      render("comments/new.html.erb")
+      redirect_to("/photos", :notice => "Comment couldnt be created")
     end
   end
 
--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -1,46 +1,39 @@
 class LikesController < ApplicationController
   def index
     @likes = Like.all
-
     render("likes/index.html.erb")
   end
 
   def show
     @like = Like.find(params[:id])
-
     render("likes/show.html.erb")
   end
 
   def new
     @like = Like.new
-
+    redirect_to("/photos")
     render("likes/new.html.erb")
   end
 
   def create
     @like = Like.new
-
-    @like.user_id = params[:user_id]
+    @like.user_id = current_user.id
     @like.photo_id = params[:photo_id]
-
     save_status = @like.save
-
     if save_status == true
-      redirect_to("/likes/#{@like.id}", :notice => "Like created successfully.")
+      redirect_to("/photos", :notice => "Like created successfully.")
     else
-      render("likes/new.html.erb")
+      render("/photos", :notice => "Couldnt like photo")
     end
   end
 
   def edit
     @like = Like.find(params[:id])
-
     render("likes/edit.html.erb")
   end
 
   def update
     @like = Like.find(params[:id])
-
     @like.user_id = params[:user_id]
     @like.photo_id = params[:photo_id]
 
@@ -55,9 +48,7 @@ class LikesController < ApplicationController
 
   def destroy
     @like = Like.find(params[:id])
-
     @like.destroy
-
     if URI(request.referer).path == "/likes/#{@like.id}"
       redirect_to("/", :notice => "Like deleted.")
     else
